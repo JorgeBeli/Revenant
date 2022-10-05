e9206b88546c3bd3485f94747332652b4d888c6b
JorgeBeli <shorshbeliera@gmail.com>
2022-10-05 14:41:56 -0300

    adding routes

--- src/App.js
+++ src/App.js
@@ -12,15 +12,15 @@ function App() {
       <header className='navbar__wraper'>
         <BrowserRouter>
           <Navbar/>
-          <Routes>
-            <Route path='/' element={<Intro/>} />
-            <Route path='/products' element={<Products/>}/>
-            <Route path='/products/item/:id' element={<IndividualProduct/>}></Route>
-            <Route path='/aboutUs' element={<Intro/>}></Route>
-            <Route path='/location' element={<Location/>}></Route>
-            <Route path='/cart' element={<Intro/>}></Route>
-            <Route path='/user' element={<Intro/>}></Route>
-          </Routes>
+            <Routes>
+              <Route path='/' element={<Intro/>} />
+              <Route path='/products' element={<Products/>}/>
+              <Route path='/products/item/:id' element={<IndividualProduct/>}></Route>
+              <Route path='/aboutUs' element={<Intro/>}></Route>
+              <Route path='/location' element={<Location/>}></Route>
+              <Route path='/cart' element={<Intro/>}></Route>
+              <Route path='/user' element={<Intro/>}></Route>
+            </Routes>
         </BrowserRouter>
       </header>
       <main>
